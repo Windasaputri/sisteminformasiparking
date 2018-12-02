7ba50851fa5cdb2713e8f376656b8f61fbe70faa
Windasaputri <windasaputri697@gmail.com>
2018-12-02 20:54:00 +0700

    menambahkan file loop.php

---
+++ loop.php
@@ -0,0 +1,18 @@
+
+<html>
+<head>
+	<title>Perulangan</title>
+</head>
+<body>
+	<form method="POST">
+		<h3>Aplikasi Perulangan</h3>
+		<hr/>
+		Masukan Text:<br/>
+		<input type="text" name="teks"/><br/>
+		Jumlah Baris<br/>
+		<input type="text" name="jml"/><br/>
+		<input type="submit" name="submit" value="proses" />
+	</form?
+	
+</body>
+</html>
